0f9942700ad3fc2ef0d980c7ece97e6b4b9d8e97
marce <deisycuellarg@gmail.com>
2021-03-20 21:51:24 -0500

    proyecto conexion web service

---
+++ ClassConexionDB/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassConexionDB
+{
+    public class Utilidades
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+            SqlConnection con = new SqlConnection("Data Source = (localdb)\\marcela; Initial Catalog = conexionVS; Integrated Security = True");
+            con.Open();
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter dp = new SqlDataAdapter(cmd, con);
+            dp.Fill(ds);
+            con.Close();
+
+            return ds;
+        }
+    }
+
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using ClassConexionDB;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cmd = string.Format("select * from usuarios where account = '{0}' and password = '{1}'", textBox1.Text.Trim(), textBox2.Text.Trim());
+                DataSet ds = Utilidades.Ejecutar(cmd);
+
+                string cuenta = ds.Tables[0].Rows[0]["account"].ToString().Trim();
+                string password = ds.Tables[0].Rows[0]["password"].ToString().Trim();
+                
+                if(cuenta == textBox1.Text.Trim() && password == textBox2.Text.Trim()) {
+                    //MessageBox.Show("Conectado a la DB");
+                    this.Hide();
+                    Form2 f = new Form2();
+                    f.ShowDialog();
+                    
+                }            
+            }
+            catch(Exception ex) {
+                MessageBox.Show("No se realizo la conexion " + ex.Message);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
